b0127e6303841eb6fb82a46269e3033dd240edae
otalorajuand <otalorajuand@gmail.com>
2022-09-07 20:19:08 +0000

    Addition of task 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,6 +11,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t height_left = 0;
 	size_t height_right = 0;
 
+	if (!tree)
+		return (0);
+
 	height_left = tree->left ? binary_tree_height(tree->left) + 1 : 0;
 	height_right = tree->right ? binary_tree_height(tree->right) + 1 : 0;
 
