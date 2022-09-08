83b3b1d403b69ac91445e6deda75de0362f8f751
otalorajuand <otalorajuand@gmail.com>
2022-09-08 02:18:53 +0000

    addition of task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,12 +1,23 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_leaves - measures the number of leaves of a binary tree.
+ * @tree: The tree to be measured.
+* Return: The leaves of the tree.
+ */
+
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-	size_t size = 0, size_left = 0, size_right = 0;
+	size_t leaves = 0, leaves_left = 0, leaves_right = 0;
 
 	if (!tree)
-		return (size);
+		return (leaves);
 
-	size_left = (!tree->left && !tree->right) ? binary_tree_leaves(tree) : 1;
+	if (!tree->left && !tree->right)
+		return (leaves + 1);
 
+	leaves_left = tree->left ? binary_tree_leaves(tree->left) : 0;
+	leaves_right = tree->right ? binary_tree_leaves(tree->right) : 0;
+
+	return (leaves_left + leaves_right);
 }
